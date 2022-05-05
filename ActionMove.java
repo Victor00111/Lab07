8d77641d23a9b5e8fa743aed11ba1c6a47e58bf1
Zach <81524537+SamuraiZach@users.noreply.github.com>
2022-05-05 17:37:03 -0400

    Update ActionMove.java

--- ActionMove.java
+++ ActionMove.java
@@ -1,3 +1,17 @@
+/**
+ * <h1>Lab 7 </h1>
+ * 
+ * <h2>CISC 181-40L Spring 2022</h2>
+ * 
+ * <h3>University of Delaware</h3>
+ * 
+ * <p>
+ * // Abstract class that handles the move feature and extends to Action
+ * 
+ * @author Zachariah Barraza, Cam Kennedy, and Victor Tung
+ * 
+ * @since (2022-05-05)
+ */
 public abstract class ActionMove extends Action{
 
     public ActionMove(GameS22 game, int row, int col, int mRow, int mCol) {
