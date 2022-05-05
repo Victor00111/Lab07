c46937522eec7734481b6eaeabcd8b586b7c6e68
Zach <81524537+SamuraiZach@users.noreply.github.com>
2022-05-05 17:41:17 -0400

    Update Recruiter.java

--- Recruiter.java
+++ Recruiter.java
@@ -1,3 +1,17 @@
+/**
+ * <h1>Lab 7 </h1>
+ * 
+ * <h2>CISC 181-40L Spring 2022</h2>
+ * 
+ * <h3>University of Delaware</h3>
+ * 
+ * <p>
+ * // Interface for recruiting
+ * 
+ * @author Zachariah Barraza, Cam Kennedy, and Victor Tung
+ * 
+ * @since (2022-05-05)
+ */
 public interface Recruiter {
     public abstract int getNumRecruits();
     public abstract void setNumRecruits(int x);
