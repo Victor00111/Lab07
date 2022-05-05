6b7c7047676ca636e801494e045c923a12eea840
Zach <81524537+SamuraiZach@users.noreply.github.com>
2022-05-05 17:39:24 -0400

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -1,3 +1,17 @@
+/**
+ * <h1>Lab 7 </h1>
+ * 
+ * <h2>CISC 181-40L Spring 2022</h2>
+ * 
+ * <h3>University of Delaware</h3>
+ * 
+ * <p>
+ * // The controller class that handles the game
+ * 
+ * @author Zachariah Barraza, Cam Kennedy, and Victor Tung
+ * 
+ * @since (2022-05-05)
+ */
 import java.util.ArrayList;
 
 public class Controller {
