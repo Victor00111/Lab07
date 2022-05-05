e01a86afaa0f9e5c00a9e85b4e1f555bf49600d8
Zach <81524537+SamuraiZach@users.noreply.github.com>
2022-05-05 17:40:52 -0400

    Update GameS22.java

--- GameS22.java
+++ GameS22.java
@@ -1,3 +1,17 @@
+/**
+ * <h1>Lab 7 </h1>
+ * 
+ * <h2>CISC 181-40L Spring 2022</h2>
+ * 
+ * <h3>University of Delaware</h3>
+ * 
+ * <p>
+ * // Winner qualifications for Game
+ * 
+ * @author Zachariah Barraza, Cam Kennedy, and Victor Tung
+ * 
+ * @since (2022-05-05)
+ */
 public class GameS22 extends Game{
     public GameS22(int rows, int cols, Team team1, Team team2){
         super(rows,cols,team1,team2);
