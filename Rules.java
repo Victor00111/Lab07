0990b84c462e4f3a677d7b9e8df571c495d75ab4
Victor00111 <103544144+Victor00111@users.noreply.github.com>
2022-05-20 19:21:47 -0400

    Update Rules.java

--- Rules.java
+++ Rules.java
@@ -12,99 +12,95 @@
  *
  * @since (2022-05-05)
  */
-import java.util.ArrayList;
-
 public class Rules {
-    //Not Done Yet!
 
-    GameS22 game;
-    int row1;
-    int col1;
-    int row2;
-    int col2;
-    char action;
-
-    public static boolean checkValidAction(GameS22 game, int row1, int col1, int row2, int col2, char action){
-        boolean tmp = false;
-        //Simplify comparing main square
-        Piece check = game.getGameBoard().getSpaces()[row1][col1].getPiece();
-        Piece otherPiece = game.getGameBoard().getSpaces()[row2][col2].getPiece();
-
-
-        //For checking pieces
-        PieceBuzz Buzz = new PieceBuzz();
-        PieceBlueHen Blue = new PieceBlueHen();
-        PieceEvilMinion Evil = new PieceEvilMinion();
-        PieceMinion Minion = new PieceMinion();
-
-        //simplify checks
-        Team other = game.getOpponentTeam();
-        Team current = game.getCurrentTeam();
-
-        //To check can attacks
-        PieceBuzz BuzzLasor = (PieceBuzz) game.getGameBoard().getSpaces()[row1][col1].getPiece();
-        PieceEvilMinion EvilHunger = (PieceEvilMinion) game.getGameBoard().getSpaces()[row1][col1].getPiece();
-
-        //Checking teams
-        String currTeam = game.getCurrentTeam().getTeamColor();
-        String oppTeam = game.getOpponentTeam().getTeamColor();
-        String mainTeam = game.getGameBoard().getSpaces()[row1][col1].getSquareColor();
-        String otherTeam = game.getGameBoard().getSpaces()[row2][col2].getSquareColor();
-
-
-
-
-        if(!game.board.inBounds(row1, col1) || !game.board.inBounds(row2, col2) && !check.validMovePath(row1,col1,row2,col2)){
-            tmp = false;
-        }
-        if(!current.equals(other) || !game.isTurn(game.getCurrentTeam())){
-            tmp = false;
-        }
-        else if(action == 'M' && currTeam == mainTeam){
-            if(otherPiece == null ){
-                tmp = true;
-            }
-        }
-        else if(action == 'S' && currTeam == mainTeam){
-            if(!check.equals(Buzz)){
-                if(otherPiece == null){
-                    tmp = true;
+    public static boolean checkValidAction(GameS22 gameS22, int row1, int col1, int row2, int col2, char action) {
+        // New Rule Modification -
+        if (action == 'B' || action == 'b') {
+            if (gameS22.getGameBoard().inBounds(row1, col1) && gameS22.getGameBoard().inBounds(row2, col2)) {
+                if (!(gameS22.getGameBoard().getSquares()[row1][col1].isEmpty())) {
+                    if (gameS22.turn.equals(gameS22.getGameBoard().getSquares()[row1][col1].getPiece().teamColor)) {
+                        if (gameS22.getGameBoard().getSquares()[row1][col1].getPiece().symbol == 'N' || gameS22.getGameBoard().getSquares()[row1][col1].getPiece().symbol == 'B') {
+                            if ((row2 - row1 <= 4 || row1 - row2 <= 4) && (col2 - col1 <= 4 || col1 - col2 <= 4)) {
+                                return true;
+                            }
+                        }
+                    }
                 }
             }
-
         }
-        else if(action == 'R' && currTeam == mainTeam){
-            if(check.equals(Buzz)){
-                if(otherPiece != null){
-                    tmp = true;
+
+        if (action == 'M' || action == 'm') {
+            if (gameS22.getGameBoard().inBounds(row1, col1) && gameS22.getGameBoard().inBounds(row2, col2)) {
+                if (!(gameS22.getGameBoard().getSquares()[row1][col1].isEmpty())) {
+                    if (gameS22.getGameBoard().getSquares()[row2][col2].isEmpty()) {
+                        if (gameS22.turn.equals(gameS22.getGameBoard().getSquares()[row1][col1].getPiece().teamColor)) {
+                            if (gameS22.getGameBoard().getSquares()[row1][col1].getPiece().validMovePath(row1, col1, row2, col2)) {
+                                return true;
+                            }
+                        }
+                    }
                 }
             }
-
         }
-        else if(action == 'A'){
-            if(check.equals(Buzz)  && BuzzLasor.canAttack()){
-                if(oppTeam == otherTeam){
-                    tmp = true;
-                }
-            }
-            if(check.equals(Blue) && currTeam == mainTeam){
-                if(oppTeam == otherTeam){
-                    tmp = true;
-                }
-            }
-            if(check.equals(Evil) && EvilHunger.canAttack()){
-                if(otherPiece.equals(Minion) && oppTeam == mainTeam){
-                    tmp = true;
-                }
-                else if(oppTeam == otherTeam){
-                    tmp = true;
-                }
 
+        if (action == 'S' || action == 's') {
+            if (gameS22.getGameBoard().inBounds(row1, col1) && gameS22.getGameBoard().inBounds(row2, col2)) {
+                if (!(gameS22.getGameBoard().getSquares()[row1][col1].isEmpty())) {
+                    if (gameS22.getGameBoard().getSquares()[row2][col2].isEmpty()) {
+                        if (gameS22.turn.equals(gameS22.getGameBoard().getSquares()[row1][col1].getPiece().teamColor)) {
+                            if (gameS22.getGameBoard().getSquares()[row1][col1].getPiece().validMovePath(row1, col1, row2, col2)) {
+                                if (!(gameS22.getGameBoard().getSquares()[row1][col1].getPiece().symbol == 'B')) {
+                                    return true;
+                                }
+                            }
+                        }
+                    }
+                }
             }
-
         }
-        return tmp;
+
+        if (action == 'R' || action == 'r') {
+            if (gameS22.getGameBoard().inBounds(row1, col1) && gameS22.getGameBoard().inBounds(row2, col2)) {
+                if (!(gameS22.getGameBoard().getSquares()[row1][col1].isEmpty())) {
+                    if (!(gameS22.getGameBoard().getSquares()[row2][col2].isEmpty())) {
+                        if (!(gameS22.getGameBoard().getSquares()[row2][col2].getPiece().teamColor.equals(gameS22.getCurrentTeam().getTeamColor()))) {
+                            if (gameS22.turn.equals(gameS22.getGameBoard().getSquares()[row1][col1].getPiece().teamColor)) {
+                                if (gameS22.getGameBoard().getSquares()[row1][col1].getPiece().validMovePath(row1, col1, row2, col2)) {
+                                    if (!(gameS22.getGameBoard().getSquares()[row1][col1].getPiece().symbol == 'B')) {
+                                        return true;
+                                    }
+                                }
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        if (action == 'A' || action == 'a') {
+            if (gameS22.getGameBoard().inBounds(row1, col1) && gameS22.getGameBoard().inBounds(row2, col2)) {
+                if (!(gameS22.getGameBoard().getSquares()[row1][col1].isEmpty())) {
+                    if (!(gameS22.getGameBoard().getSquares()[row2][col2].isEmpty())) {
+                        if (gameS22.turn.equals(gameS22.getGameBoard().getSquares()[row1][col1].getPiece().teamColor)) {
+                            if (gameS22.getGameBoard().getSquares()[row1][col1].getPiece().validAttackPath(row1, col1, row2, col2)) {
+                                if (gameS22.getGameBoard().getSquares()[row1][col1].getPiece().canAttack()) {
+                                } if (gameS22.getGameBoard().getSquares()[row1][col1].getPiece().symbol == 'H' || gameS22.getGameBoard().getSquares()[row1][col1].getPiece().symbol == 'B' || gameS22.getGameBoard().getSquares()[row1][col1].getPiece().symbol == 'S' || gameS22.getGameBoard().getSquares()[row1][col1].getPiece().symbol == 's' ) {
+                                    if (!(gameS22.getGameBoard().getSquares()[row2][col2].getPiece().teamColor.equals(gameS22.getGameBoard().getSquares()[row1][col1].getPiece().teamColor))) {
+                                        return true;
+                                    }
+                                } else if (gameS22.getGameBoard().getSquares()[row1][col1].getPiece().symbol == 'E' || gameS22.getGameBoard().getSquares()[row1][col1].getPiece().symbol == 'e') {
+                                    return true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println("\n**************************************************************************\nThis is not a valid move.\n**************************************************************************\n ");
+        System.out.println(gameS22.toString());
+        return false;
     }
-
-
 }
